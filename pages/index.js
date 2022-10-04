d0f1a54721c3b707403cfcfaa78c16fbab7d730a
TiagoLuis <111128607+TiagoLuis1@users.noreply.github.com>
2022-10-04 11:48:58 -0300

    first

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
+    <>
     <h1>oi</h1>
+<hr/>
+<Link href='/http://www,google.com.br'><a>GOOGLE</a></Link>
+</>
+
   )
 }
